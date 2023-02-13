5b04d83c071a90668d4c2e608cc857e31c87e4d6
yjsprout <ilbnmyj23@gmail.com>
2023-02-14 07:21:01 +0900

    git commit

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 
 from datetime import datetime
 import sqlite3
+import itertools
 
 bot = commands.Bot(command_prefix="!", intents = discord.Intents.default())
 TOKEN = "MTA2MjkxNjU2NDMzNDE0OTczNA.G6QQ23.evo2wsorKEKq-4lXwHQKEwMHk7IbROV1X0DfZs"
@@ -18,17 +19,19 @@ async def on_ready():
 
 @bot.tree.command(name="출석체크")
 async def check(interaction: discord.Interaction):
-    date_time = datetime.today().strftime('%Y-%m-%d %H:%M')
-    await interaction.response.send_message(f"{interaction.user.display_name} 출석했습니다.\n{date_time}")
+    date_rec = datetime.today().strftime('%Y-%m-%d')
+    time_rec = datetime.today().strftime('%H:%M')
+
+    await interaction.response.send_message(f"{interaction.user.display_name} 출석했습니다.\n{date_rec} {time_rec}")
     # user.name -> 실제 사용자 이름
     # user.display_name -> 서버에서 설정한 별명
 
     conn = sqlite3.connect('Attendance.db')
     cur = conn.cursor()
-    sql1 = "CREATE TABLE IF NOT EXISTS attTBL(name text,date_time text);"
-    sql2 = "INSERT INTO attTBL(name,date_time) values (?,?);"
+    sql1 = "CREATE TABLE IF NOT EXISTS attTBL(name text,date text, time text);"
+    sql2 = "INSERT INTO attTBL(name,date,time) values (?,?,?);"
     cur.execute(sql1)
-    cur.execute(sql2, (interaction.user.display_name, date_time))
+    cur.execute(sql2, (interaction.user.display_name, date_rec, time_rec))
     conn.commit()
     cur.close()
 
@@ -42,10 +45,10 @@ async def db(interaction: discord.Interaction):
     conn = sqlite3.connect('Attendance.db')
     cur = conn.cursor()
     cur.execute('SELECT * FROM attTBL')
-    lrow=[]
+    db_list = []
     for row in cur:
-        lrow.append(list(row))
-    await interaction.response.send_message(f"{lrow}")
+        db_list.append(list(row))
+    await interaction.response.send_message(f"{db_list}")
     cur.close()
 
 @bot.tree.command(name="resetdb")
@@ -55,6 +58,23 @@ async def reset(interaction:discord.Interaction):
     sql3 = "DROP TABLE IF EXISTS attTBL"
     cur.execute(sql3)
     cur.close()
-    await interaction.response.send_message(f"초기화")
+    await interaction.response.send_message(f"데이터베이스 초기화를 완료하였습니다.")
+
+@bot.tree.command(name="absentees")
+async def checkAbs(interaction:discord.Interaction):
+    conn = sqlite3.connect('Attendance.db')
+    cur = conn.cursor()
+    sql4 = "SELECT name FROM attTBL"
+    cur.execute(sql4)
+    appeared = []
+    for row in cur:
+        appeared.append(list(row))
+    appeared2 = list(itertools.chain(*appeared))
+    members = ['김세연', '김주미', '양진']
+    for i in appeared2:
+        members.remove(i)
+    absent = members
+    await interaction.response.send_message(f"출석 하지 않은 분들 명단 : {absent}")
+    cur.close()
 
 bot.run(TOKEN)
